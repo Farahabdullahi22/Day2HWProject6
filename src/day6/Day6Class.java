be82a73b7a0afa38e603db44adbc220da05f37ba
farahabdullahi <farahabdullahi@192.168.4.25>
2023-04-08 11:33:25 -0400

    this is project 6 final

--- src/day6/Day6Class.java
+++ src/day6/Day6Class.java
@@ -5,6 +5,13 @@ public class Day6Class {
 	public static void main(String[] args) {
 		
 		System.out.println(" to is saturday...");
+		
+		
 	}
 
+	
+	public void finalPractice() {
+		
+		System.out.println("Final practice is done ");
+	}
 }
